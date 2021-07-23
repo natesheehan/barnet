d36144dc3447caf27f643c7ab3605450fca686da
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-07-23 15:15:49 +0100

    Update barnet.r

--- barnet.r
+++ barnet.r
@@ -11,5 +11,7 @@ rnet$m_cycled_per_working_day = rnet$segment_length * rnet$bicycle
 # Get road network for preselected region
 zones = pct::get_pct_zones(region = "london")
 zones = zones %>% filter(lad_name == "Barnet")
+cycled_m_per_zone = aggregate(rnet["m_cycled_per_working_day"], zones, FUN = sum)
 
-sf::write_sf(cycled_m_per_zone,"data/shapefile/barnet-roads.shp")
+cycled_m_per_zone2 = st_transform(cycled_m_per_zone, 27700)
+sf::write_sf(cycled_m_per_zone2,"data/barnet-roads-british.shp")
